fb29f9cb37f4dcd6bea56799646294d60679c1ba
Avanire <drtony@yandex.ru>
2023-03-08 20:10:34 +0600

    redux init and control panel component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ControlPanel from "../ControlPanel/ControlPanel";
 
 function App() {
   return (
-    <main className={`w-[33.7rem] mx-auto`}>
-
+    <main className={`w-[33.7rem] mx-auto mt-10`}>
+      <div className={`mb-[1.875rem]`}><ControlPanel /></div>
     </main>
   );
 }
