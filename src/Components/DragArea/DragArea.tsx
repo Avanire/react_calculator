f0f4769bb8355beaafdfa57be86a9610cd309e9a
Avanire <60422131+Avanire@users.noreply.github.com>
2023-03-10 16:35:43 +0600

    drag to constructor v2

--- src/Components/DragArea/DragArea.tsx
+++ src/Components/DragArea/DragArea.tsx
@@ -1,17 +1,16 @@
-import React, {FC} from "react";
+import React, {Component, FC, ReactElement} from "react";
 import dropIcon from '../../images/drop-area-icon.svg';
 import {useDrop} from "react-dnd";
 import {addCalcPart} from "../../services/actions/calculator";
 import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-import {serialize, deserialize} from 'react-serialize';
+import { renderToString } from 'react-dom/server'
+import {Interweave} from "interweave";
 
 const DragArea: FC = () => {
     const dispatch = useAppDispatch();
     const constructor = useAppSelector(state => state.calculatorConstructor.constructor);
-
-    const [, drop] = useDrop({
+    console.log(constructor)
+    const [{isOver}, drop] = useDrop({
         accept: 'Component',
         collect: (monitor) => ({
             isOver: monitor.isOver(),
@@ -20,7 +19,7 @@ const DragArea: FC = () => {
         drop(item) {
             dispatch({
                 type: addCalcPart.type,
-                payload: serialize(item)
+                payload: renderToString(item)
             });
         }
     });
@@ -28,10 +27,12 @@ const DragArea: FC = () => {
     return (
         constructor.length > 0 ? (
             <div className={`flex flex-col space-y-3`} ref={drop}>
-
+                {
+                    constructor.map(item => <Interweave content={item} />)
+                }
             </div>
         ) : (
-            <section className={`border-2 border-dashed border-borderDashedColor rounded-md flex items-center justify-center`} ref={drop} /*style={{ backgroundColor: isOver ? '#F0F9FF' : 'white' }}*/>
+            <section className={`border-2 border-dashed border-borderDashedColor rounded-md flex items-center justify-center`} ref={drop} style={{ backgroundColor: isOver ? '#F0F9FF' : 'white' }}>
                 <div className={`w-32 text-center`}>
                     <img src={dropIcon} alt='Перетащите сюда любой элемент из левой панели' className={`mb-3 mx-auto`} />
                     <div className={`text-iris-100 text-sm mb-1`}>Перетащите сюда</div>
