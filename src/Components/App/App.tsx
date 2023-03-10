602118f527d480509c6549b04fdef7bca4ef808e
Avanire <60422131+Avanire@users.noreply.github.com>
2023-03-10 13:10:06 +0600

    redux and dnd init

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -5,22 +5,26 @@ import CalculatorOperations from "../CalculatorOperations/CalculatorOperations";
 import CalculatorNumberPanel from "../CalculatorNumberPanel/CalculatorNumberPanel";
 import CalculatorEqualButton from "../CalculatorEqualButton/CalculatorEqualButton";
 import DragArea from "../DragArea/DragArea";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 function App() {
-  return (
-    <main className={`w-[33.7rem] mx-auto mt-10`}>
-        <div className={`mb-[1.875rem]`}><ControlPanel /></div>
-        <section className={`grid grid-cols-2 gap-x-[3.75rem]`}>
-            <div className={`flex flex-col space-y-3`}>
-                <CalculatorResult />
-                <CalculatorOperations />
-                <CalculatorNumberPanel />
-                <CalculatorEqualButton />
-            </div>
-            <DragArea />
-        </section>
-    </main>
-  );
+    return (
+        <main className={`w-[33.7rem] mx-auto mt-10`}>
+            <div className={`mb-[1.875rem]`}><ControlPanel/></div>
+            <DndProvider backend={HTML5Backend}>
+                <section className={`grid grid-cols-2 gap-x-[3.75rem]`}>
+                    <div className={`flex flex-col space-y-3`}>
+                        <CalculatorResult/>
+                        <CalculatorOperations/>
+                        <CalculatorNumberPanel/>
+                        <CalculatorEqualButton/>
+                    </div>
+                    <DragArea/>
+                </section>
+            </DndProvider>
+        </main>
+    );
 }
 
 export default App;
